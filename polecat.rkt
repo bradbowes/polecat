5ff5478762be800e3178fdff307d26acd2d60320
Brad Bowes <bbowes.brad@gmail.com>
2024-06-05 10:10:59 -0600

    tests

--- polecat.rkt
+++ polecat.rkt
@@ -8,15 +8,14 @@
        (module-data `(module polecat-module "polecat.rkt" ,@data)))
     (datum->syntax #f module-data)))
 
+#|
 (define-syntax-rule (module-begin form ...)
   (#%module-begin
      form ...))
 
 (provide (rename-out [module-begin #%module-begin]))
+|#
+
+(provide #%module-begin)
 (provide read-syntax define letrec lambda if begin + - * / = < > >= <= void #%app #%datum list first rest cons)
 (provide (rename-out [display print] [displayln println] [null? empty]))
-
-
-
-
-
